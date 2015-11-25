8b9bccaa05fd51104f081ed6e66a97af45045bf3
Eric Allen <decoy@internetalche.me>
2015-11-25 11:41:54 -0500

    Added basic constructor and a general selector method
    Basic mimicing of a very simplified version of jQuery's `$` method.

---
+++ src/vanilla.js
@@ -0,0 +1,27 @@
+
+    /**
+        Vanilla
+
+        A basic JavaScript Library that will mimic some jQuery funcitonality
+        and allow us to explore some of the ways that jQuery achieves things
+        as well as illustrating places where we really might not need jQuery
+        to perform certain actions.
+    */
+
+    //this will be the basis of our library, much like jQuery's `jQuery` function (often aliased as `$`)
+    var $v = function(selector) {
+        //if this constructor was called without the `new` keyword, we should do that
+        if(this === window) {
+            return new $v(selector);
+        }
+
+        //instead of a complicated selector engine like Sizzle, we'll just be relying on `document.querySelectorAll()`
+        //we are using `[].slice.call()` to turn the Array-like list of HTML Nodes into an actual array that we can use native Array methods on
+        this.elements = [].slice.call(document.querySelectorAll(selector));
+
+        //return our object so that we can chain methods from our prototype
+        return this;
+    };
+
+    //set up our prototype and alias it is `$v.fn` much like jQuery has `jQuery.fn` (often aliased as `$.fn`)
+    $v.fn = $v.prototype = {};
