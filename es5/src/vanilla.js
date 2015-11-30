e9edfd1bdcca6a0d71d675b27f62596121265b4d
Eric Allen <decoy@internetalche.me>
2015-11-30 16:39:41 -0500

    Added `attr`, `data`, and `extend` methods to es5 and es2015 implementations

--- es5/src/vanilla.js
+++ es5/src/vanilla.js
@@ -35,7 +35,7 @@
     $v.fn.utils.convertSelectorToString = function(str) {
         var checkCharacter = str[0];
 
-        switch(checkChracter) {
+        switch(checkCharacter) {
             case '.':
             case '#':
                 return str.substr(1);
@@ -46,7 +46,7 @@
 
     //this utility will take an element and split it's `className` attribute into an array
     $v.fn.utils.getClassList = function(element) {
-        return this.utils.convertDelimitedStringToArray(element.className);
+        return this.convertDelimitedStringToArray(element.className);
     };
 
     //this utility will take an array of classes and return the joined class list
@@ -65,7 +65,7 @@
             return (index ? atIndex : true);
         }
 
-        return false;
+        return (index ? atIndex : false);
     };
 
     //this utility takes a string and converts it into an array
@@ -84,11 +84,96 @@
 
         //iterate through our items array and trim any strings
         //then return the new array
-        return items.forEach(function(item) {
+        items.forEach(function(item, index, array) {
             if(typeof item === 'string') {
-                item = item.trim();
+                array[index] = item.trim();
             }
         });
+
+        return items;
+    };
+
+    //this utility will take a node and return an object of it's attributes and values
+    $v.fn.utils.generateAttributesObject = function(element, data) {
+        //intialize empty object to store our attribute values
+        var returnAttrs = {};
+
+        //cache the attribute node list from our element so it's easier to reference
+        var elementAttrs = element.attributes;
+
+        //if the data attribute wasn't provided, explicitly set it to false
+        if(typeof data === 'undefined') {
+            data = false;
+        }
+
+        //iterate through our attributes node list
+        //we are using a `for()` loop because this node list is an Array-like object and not an actual Array
+        for(var i = 0; i < elementAttrs.length; i++) {
+            //if we are only looking for data attributes and this is one, or if we aren't only looking for data attributes
+            if(data && elementAttrs[i].nodeName.indexOf('data-') === 0 || !data) {
+                //add attribute: value pair to our attribute object
+                returnAttrs[elementAttrs[i].nodeName] = elementAttrs[i].nodeValue;
+            }
+        }
+
+        //return our object of attributes
+        return returnAttrs;
+    };
+
+    //this utility will take a container object and extend it with another object
+    //`deep` is a Boolean that tells the utility whether it should copy deeply or not
+    $v.fn.utils.mergeObjects = function(container, extendWith, deep) {
+        //if we need to do a deep merge, return the deepMergeObjects() utility method
+        if(deep) {
+            return this.utils.deepMergeObjects(container, extendWith);
+        }
+
+        //iterate through properties in `extendWith` object
+        for(var prop in extendWith) {
+            //make sure this is an enumerable property
+            if(extendWith.hasOwnProperty(prop)) {
+                //set the value of our container object's property to the value of our extension object's property
+                container[prop] = item[prop];
+            }
+        }
+
+        //return our extended object
+        return container;
+    };
+
+    //this utility will take a container object and extend it with another object
+    //this is a deep copy
+    $v.fn.utils.deepMergeObjects = function(container, extendWith) {
+        //iterate through properties in `extendWith` object
+        for(var prop in extendWith) {
+            //check to see if this is an enumerable property of our extension object
+            if(extendWith.hasOwnProperty(prop)) {
+                //check to see if this is an enumerable proerty of our container object
+                //if this property already exists on our container, we will want to treat it a bit differently
+                if(container.hasOwnProperty(prop)) {
+                    //if this property is an object
+                    if(typeof extendWith[prop] === 'object') {
+                        //recursively deep merge the object before setting our property's value
+                        container[prop] = this.fn.utils.deepMergeObjects((container[prop] ? container[prop] : {}), extendWith[prop]);
+                    //otherwise, we'll just set the container object's property to the value of the extension object's property
+                    } else {
+                        container[prop] = extendWith[prop];
+                    }
+                //if this property didn't exist on our container
+                } else {
+                    //define a new property on our container object with this proprety's value from our extension object
+                    Object.defineProperty(container, prop, {
+                        value: extendWith[prop],
+                        writable: true,
+                        enumerable: true,
+                        configurable: true
+                    });
+                }
+            }
+        }
+
+        //return our extended container object
+        return container;
     };
 
     //replaces the current collection with a new collection created by running `querySelectorAll()` from each item in the collection and looking for
@@ -99,7 +184,7 @@
         this.elements = [];
 
         elements.forEach(function(item) {
-            this.elements.concat([].slice.call(item.querySelectorAll(str)));
+            this.elements = this.elements.concat([].slice.call(item.querySelectorAll(str)));
         }, this);
 
         return this;
@@ -111,20 +196,14 @@
     //2: We will accept space, or comma-delimited lists of class names for each parameter
     $v.fn.hasClass = function() {
         //declare variable we'll use to store an array of class names
-        var classes;
-
-        //initialize empty array to store our arguments
-        var args = [];
+        var classes = [];
 
         //convert our arguments Array-like object into an actual array we can work with
         for(var a = 0; a < arguments.length; a++) {
-            args.push(arguments[a]);
+            this.utils.convertDelimitedStringToArray(arguments[a]).forEach(function(item) {
+                classes.push(item);
+            }, this);
         }
-
-        //iterate through our new args array and split out any comma or space-delimited lists of class names
-        classes = args.map(function(item) {
-            return this.utils.convertDelimitedStringToArray(item);
-        }, this);
 
         //iterate through the classes we need to check for
         //we are using `.every()` so that we can break out of the loop when the class is not found on an element
@@ -147,20 +226,14 @@
     //2: We will accept space, or comma-delimited lists of class names for each parameter
     $v.fn.addClass = function() {
         //declare variable we'll use to store an array of class names
-        var classes;
-
-        //initialize empty array to store our arguments
-        var args = [];
+        var classes = [];
 
         //convert our arguments Array-like object into an actual array we can work with
         for(var a = 0; a < arguments.length; a++) {
-            args.push(arguments[a]);
+            this.utils.convertDelimitedStringToArray(arguments[a]).forEach(function(item) {
+                classes.push(item);
+            }, this);
         }
-
-        //iterate through our new args array and split out any comma or space-delimited lists of class names
-        classes = args.map(function(item) {
-            return this.utils.convertDelimitedStringToArray(item);
-        }, this);
 
         //iterate through the classes we need to check for
         //we are using `.every()` so that we can break out of the loop when the class is not found on an element
@@ -173,7 +246,10 @@
 
                 //check to see if our this item already has the class, add it if not
                 if(!this.utils.arrayContains(classList, this.utils.convertSelectorToString(str))) {
-                    array[index].className = this.utils.createClassList(classList.push(str));
+                    //add it to the class list
+                    classList.push(this.utils.convertSelectorToString(str));
+
+                    array[index].className = this.utils.createClassList(classList);
                 }
             }, this);
         }, this);
@@ -188,20 +264,14 @@
     //2: We will accept space, or comma-delimited lists of class names for each parameter
     $v.fn.removeClass = function() {
         //declare variable we'll use to store an array of class names
-        var classes;
-
-        //initialize empty array to store our arguments
-        var args = [];
+        var classes = [];
 
         //convert our arguments Array-like object into an actual array we can work with
         for(var a = 0; a < arguments.length; a++) {
-            args.push(arguments[a]);
+           this.utils.convertDelimitedStringToArray(arguments[a]).forEach(function(item) {
+                classes.push(item);
+           }, this);
         }
-
-        //iterate through our new args array and split out any comma or space-delimited lists of class names
-        classes = args.map(function(item) {
-            return this.utils.convertDelimitedStringToArray(item);
-        }, this);
 
         //iterate through the classes we need to check for
         //we are using `.every()` so that we can break out of the loop when the class is not found on an element
@@ -214,12 +284,14 @@
 
                 //check to see if class exists in class list
                 //we cache this in it's own variable so we can use it for a splice call later on
-                var removeIndex = this.utils.arrayContains(classList, str, true);
+                var removeIndex = this.utils.arrayContains(classList, this.utils.convertSelectorToString(str), true);
 
                 //if the specified class was found in this element's class list
                 if(removeIndex !== -1) {
                     //remove it from the class list
-                    array[index].className = this.utils.createClassList(classList.splice(removeIndex, 1));
+                    classList.splice(removeIndex, 1);
+
+                    array[index].className = this.utils.createClassList(classList);
                 }
             }, this);
         }, this);
@@ -234,20 +306,14 @@
     //2: We will accept space, or comma-delimited lists of class names for each parameter
     $v.fn.toggleClass = function() {
         //declare variable we'll use to store an array of class names
-        var classes;
-
-        //initialize empty array to store our arguments
-        var args = [];
+        var classes = [];
 
         //convert our arguments Array-like object into an actual array we can work with
         for(var a = 0; a < arguments.length; a++) {
-            args.push(arguments[a]);
+            this.utils.convertDelimitedStringToArray(arguments[a]).forEach(function(item) {
+                classes.push(item);
+            }, this);
         }
-
-        //iterate through our new args array and split out any comma or space-delimited lists of class names
-        classes = args.map(function(item) {
-            return this.utils.convertDelimitedStringToArray(item);
-        }, this);
 
         //iterate through the classes we need to check for
         //we are using `.every()` so that we can break out of the loop when the class is not found on an element
@@ -259,16 +325,106 @@
 
                 //check to see if class exists in class list
                 //we cache this in it's own variable so we can use it for a splice call later on
-                var toggleIndex = this.utils.arrayContains(classList, str, true);
+                var toggleIndex = this.utils.arrayContains(classList, this.utils.convertSelectorToString(str), true);
 
-                if(toggleIndex !== -1) {
-                    array[index].className = this.utils.createClassList(classList.push(str));
+                if(toggleIndex === -1) {
+                    classList.push(this.utils.convertSelectorToString(str));
                 } else {
-                    array[index].className = this.utils.createClassList(classList.splice(toggleIndex, 1));
+                    classList.splice(toggleIndex, 1);
                 }
+
+                array[index].className = this.utils.createClassList(classList);
             }, this);
         }, this);
 
         //return reference to `$v` for method chaining
         return this;
+    };
+
+    //method for getting/setting an attribute's value
+    //if only the attribute parameter is provided, returns the attributes value from the first item in the collection
+    //if both parameters are provided, sets the attribute to the provided value for each element in the collection
+    //if the value provided is equal to `null`, removes the attribute for each element in the collection
+    //if both the attribute and value are omitted, returns value of all attributes from the first item in the collection
+    $v.fn.attr = function(attribute, value, data) {
+        //if this wasn't called from the `$v.data()` method
+        if(typeof data === 'undefined') {
+            //explicitly define `data` as false
+            data = false;
+        }
+
+        //if value was provided
+        if(typeof value !== 'undefined') {
+            //iterate through collection of elements
+            this.elements.forEach(function(item, index, array) {
+                //if the value being passed is null, we want to remove the attribute
+                if(value === null) {
+                    array[index].removeAttribute(attribute);
+                //if the value isn't null, we want to set the attribute
+                } else {
+                    array[index].setAttribute(attribute, value);
+                }
+            }, this);
+
+            //return reference to `$v` for method chaining
+            return this;
+        //if value wasn't provided
+        //we return the actual value in this case, so the getter version of this method is not eligible for any method chaining
+        } else {
+            //if attribute name was provided
+            if(typeof attribute !== 'undefined') {
+                //return value of `attribute` for first item in collection
+                return this.elements[0].getAttribute(attribute);
+            //if no attribute was provided
+            } else {
+                //return values of all attributes for first item in collection
+                return this.utils.generateAttributesObject(this.elements[0], data);
+            }
+        }
+    };
+
+    //method for getting/setting a `data-` atrribute's value
+    //calls the `$v.attr()` method, but prepends "data-" to the attribute name if necessary
+    //will return all data attributes when called without either parameter
+    $v.fn.data = function(attribute, value) {
+        //return the result of `$v.attr()` with "data-" prepended to the provided attribute name
+        return this.attr((attribute.indexOf('data-') !== 0 ? 'data-' + attribute : attribute), value, true);
+    };
+
+    //method for extending an object with any other number of objects
+    //if the first parameter is a boolean and is true, it means we want to deeply merge the objects
+    //the first object provided will be used as a container object, to prevent mutation, pass an empty object `{}` as your container
+    //we aren't defining any parameters explicitly because the parameters are fluid in this case
+    //this method returns an object and is not eligible for any method chaining
+    $v.fn.extend = function() {
+        //initialize empty array to store references to our objects that are currently in the  `arguments` object
+        var objs = [];
+
+        //initialize a boolean for whether we want to do a deep merge or not
+        //this method performs a shallow merge by default
+        var deep = false;
+
+        //iterate through our arguments Array-like object
+        for(var a = 0; a < arguments.length; a++) {
+            //add each argument to our `objs` array
+            objs[a] = arguments[a];
+        }
+
+        //if the first argument was a boolean
+        if(typeof objs[0] === 'boolean') {
+            //we'll make it our new `deep` setting and remove it from the array
+            deep = objs.shift();
+        }
+
+        //take the first object from the arguments, store it as our container, and remove it from the array
+        var container = objs.shift();
+
+        //iterate through the rest of our argument objects
+        objs.forEach(function(item) {
+            //mutate our container object by merging it with this item in the array
+            container = this.utils.mergeObjects(container, item, deep);
+        }, this);
+
+        //return the merged objects as a single object
+        return container;
     };
--- es6/src/vanilla.js
+++ es6/src/vanilla.js
@@ -84,6 +84,75 @@
         });
     };
 
+    //this utility will take a node and return an object of it's attributes and values
+    $v.fn.utils.generateAttributesObject = (element, data = false) => {
+        //intialize empty object to store our attribute values
+        let returnAttrs = {};
+
+        //cache the attribute node list from our element so it's easier to reference
+        const elementAttrs = element.attributes;
+
+        //iterate through our attributes node list
+        //we are using a `for()` loop because this node list is an Array-like object and not an actual Array
+        for(var i = 0; i < elementAttrs.length; i++) {
+            //if we are only looking for data attributes and this is one, or if we aren't only looking for data attributes
+            if(data && elementAttrs[i].nodeName.indexOf('data-') === 0 || !data) {
+                //add attribute: value pair to our attribute object
+                returnAttrs[elementAttrs[i].nodeName] = elementAttrs[i].nodeValue;
+            }
+        }
+
+        //return our object of attributes
+        return returnAttrs;
+    };
+
+    //this utility will take a container object and extend it with another object
+    //`deep` is a Boolean that tells the utility whether it should copy deeply or not
+    $v.fn.utils.mergeObjects = (container, extendWith, deep = false) => {
+        //if we need to do a deep merge, return the deepMergeObjects() utility method
+        if(deep) {
+            return this.utils.deepMergeObjects(container, extendWith);
+        }
+
+        //use Object.assign for a nice shallow merge
+        return Object.assign(container, extendWith);
+    };
+
+    //this utility will take a container object and extend it with another object
+    //this is a deep copy
+    $v.fn.utils.deepMergeObjects = (container, extendWith) => {
+        //iterate through properties in `extendWith` object
+        for(var prop in extendWith) {
+            //check to see if this is an enumerable property of our extension object
+            if(extendWith.hasOwnProperty(prop)) {
+                //check to see if this is an enumerable proerty of our container object
+                //if this property already exists on our container, we will want to treat it a bit differently
+                if(container.hasOwnProperty(prop)) {
+                    //if this property is an object
+                    if(typeof extendWith[prop] === 'object') {
+                        //recursively deep merge the object before setting our property's value
+                        container[prop] = this.fn.utils.deepMergeObjects((container[prop] ? container[prop] : {}), extendWith[prop]);
+                    //otherwise, we'll just set the container object's property to the value of the extension object's property
+                    } else {
+                        container[prop] = extendWith[prop];
+                    }
+                //if this property didn't exist on our container
+                } else {
+                    //define a new property on our container object with this proprety's value from our extension object
+                    Object.defineProperty(container, prop, {
+                        value: extendWith[prop],
+                        writable: true,
+                        enumerable: true,
+                        configurable: true
+                    });
+                }
+            }
+        }
+
+        //return our extended container object
+        return container;
+    };
+
     //replaces the current collection with a new collection created by running `querySelectorAll()` from each item in the collection and looking for
     //a specified selector
     $v.fn.find = (str) => {
@@ -221,3 +290,78 @@
         //return reference to `$v` for method chaining
         return this;
     };
+
+    //method for getting/setting an attribute's value
+    //if only the attribute parameter is provided, returns the attributes value from the first item in the collection
+    //if both parameters are provided, sets the attribute to the provided value for each element in the collection
+    //if the value provided is equal to `null`, removes the attribute for each element in the collection
+    //if both the attribute and value are omitted, returns value of all attributes from the first item in the collection
+    $v.fn.attr = function(attribute, value, data = false) {
+        //if value was provided
+        if(typeof value !== 'undefined') {
+            //iterate through collection of elements
+            this.elements.forEach( (item, index, array) => {
+                //if the value being passed is null, we want to remove the attribute
+                if(value === null) {
+                    array[index].removeAttribute(attribute);
+                //if the value isn't null, we want to set the attribute
+                } else {
+                    array[index].setAttribute(attribute, value);
+                }
+            });
+
+            //return reference to `$v` for method chaining
+            return this;
+        //if value wasn't provided
+        //we return the actual value in this case, so the getter version of this method is not eligible for any method chaining
+        } else {
+            //if attribute name was provided
+            if(typeof attribute !== 'undefined') {
+                //return value of `attribute` for first item in collection
+                return this.elements[0].getAttribute(attribute);
+            //if no attribute was provided
+            } else {
+                //return values of all attributes for first item in collection
+                return this.utils.generateAttributesObject(this.elements[0], data);
+            }
+        }
+    };
+
+    //method for getting/setting a `data-` atrribute's value
+    //calls the `$v.attr()` method, but prepends "data-" to the attribute name if necessary
+    //will return all data attributes when called without either parameter
+    $v.fn.data = (attribute, value) => {
+        //return the result of `$v.attr()` with "data-" prepended to the provided attribute name
+        return this.attr((attribute.indexOf('data-') !== 0 ? 'data-' + attribute : attribute), value, true);
+    };
+
+    //method for extending an object with any other number of objects
+    //if the first parameter is a boolean and is true, it means we want to deeply merge the objects
+    //the first object provided will be used as a container object, to prevent mutation, pass an empty object `{}` as your container
+    //we aren't defining any parameters explicitly because the parameters are fluid in this case
+    //this method returns an object and is not eligible for any method chaining
+    $v.fn.extend = (...objs) => {
+        //initialize a boolean for whether we want to do a deep merge or not
+        //this method performs a shallow merge by default
+        let deep = false;
+
+        //if the first argument was a boolean
+        if(typeof objs[0] === 'boolean') {
+            //we'll make it our new `deep` setting and remove it from the array
+            deep = objs.shift();
+        }
+
+        //take the first object from the arguments, store it as our container, and remove it from the array
+        var container = objs.shift();
+
+        //iterate through the rest of our argument objects
+        objs.forEach( (item) => {
+            //mutate our container object by merging it with this item in the array
+            container = this.utils.mergeObjects(container, item, deep);
+        });
+
+        //return the merged objects as a single object
+        return container;
+    };
+
+    export default $v;
